991d75160c9e8a1026e80dd96a11b6b6b49b5d7f
ozehavi <ozehavi@gmail.com>
2023-05-25 11:45:54 +0300

    dbtest

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const router = express.Router();
 
 // Connect to the MongoDB database
-mongoose.connect('mongodb+srv://ozehavi:Orenz123@cluster0.8pqq64f.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://ozehavi:Orenz123@cluster0.8pqq64f.mongodb.net/?retryWrites=true&w=majority/addcost', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.log('Error connecting to MongoDB', err));
 
