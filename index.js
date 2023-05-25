7459e0c80b30b786b793b34add15f699a4768a37
ozehavi <ozehavi@gmail.com>
2023-05-25 14:14:55 +0300

    dbtest

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Import packages
 const express = require("express");
-const indexRouter = require('./routes/index');
+const homeRouter = require('./routes/home');
 const addCostRouter = require('./routes/addcost');
 const reportRouter = require('./routes/report');
 const aboutRouter = require('./routes/about');
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.use('/', indexRouter);
+app.use('/', homeRouter);
 app.use('/addcost', addCostRouter);
 app.use('/report', reportRouter);
 app.use('/about', aboutRouter);
