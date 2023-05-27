9c98f5188b3ee118ff51f7fce3855e91cbfbd664
Ilyay <ilya.yaver@gmail.com>
2023-05-27 12:33:25 +0300

    report completed

--- routes/report.js
+++ routes/report.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const {Cost} = require("../models");
 
 function validateParams(user_id, year, month){
     //ToDo: validate year, month
@@ -8,26 +8,40 @@ function validateParams(user_id, year, month){
 }
 
 /* GET report page. */
-router.get('/', function(req, res, next) {
-    const { user_id, year, month } = req.body;
+router.get('/', async function(req, res, next) {
+    try{
+        const { user_id, year, month } = req.body;
 
-    // Check if any of the parameters are empty
-    if (!user_id || !year || !month) {
-        return res.status(400).json({ error: 'Missing parameters' });
+        // Check if any of the parameters are empty
+        if (!user_id || !year || !month) {
+            return res.status(400).json({ error: 'Missing parameters' });
+        }
+
+        const query = {
+            year: year,
+            month: month,
+            user_id: user_id
+        };
+
+        const costData = await Cost.find(query);
+
+        // create a report json and pre enters all categories arrays
+        const report = {};
+        global.categories.forEach(category => report[category] = []);
+
+        // Iterate over the cost data and populate the categories in the report
+        costData.forEach(reportCost => {
+            const category = reportCost.category;
+            report[category].push({day: reportCost.day, description: reportCost.description, sum: reportCost.sum});
+        });
+
+        // Send the JSON data as the response
+        res.status(200).json(report);
+
+    } catch (error) {
+        console.error('Error querying MongoDB:', error);
+        res.status(500).json({ error: 'An error occurred while retrieving the cost data.' + error });
     }
-
-    const reportJson = {
-        food: [{"day":21,"description":"chocolate in ikea","sum":20},{"day":5,"description":"milk","sum":6}],
-        health: [],
-        housing: [],
-        sport: [],
-        education: [],
-        transportation: [],
-        other: []
-    };
-
-    // Send the JSON data as the response
-    res.status(200).json(reportJson);
 });
 
 module.exports = router;
